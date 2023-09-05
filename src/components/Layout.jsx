8e7b3564c9f9334293be58825755e0059ce189c8
Priyanshu Kumar Panda <79529908+Priyanshu1609@users.noreply.github.com>
2023-09-06 00:13:12 +0530

    first commit

---
+++ src/components/Layout.jsx
@@ -0,0 +1,49 @@
+
+import * as React from 'react';
+
+import { siteConfig } from '@/constant/config';
+
+export const metadata = {
+  title: {
+    default: siteConfig.title,
+    template: `%s | ${siteConfig.title}`,
+  },
+  description: siteConfig.description,
+  robots: { index: true, follow: true },
+  icons: {
+    icon: '/favicon/favicon.ico',
+    shortcut: '/favicon/favicon-16x16.png',
+    apple: '/favicon/apple-touch-icon.png',
+  },
+  manifest: `/favicon/site.webmanifest`,
+  openGraph: {
+    url: siteConfig.url,
+    title: siteConfig.title,
+    description: siteConfig.description,
+    siteName: siteConfig.title,
+    images: [`${siteConfig.url}/images/og.jpg`],
+    type: 'website',
+    locale: 'en_US',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: siteConfig.title,
+    description: siteConfig.description,
+    images: [`${siteConfig.url}/images/og.jpg`],
+    // creator: '@th_clarence',
+  },
+  // authors: [
+  //   {
+  //     name: 'Theodorus Clarence',
+  //     url: 'https://theodorusclarence.com',
+  //   },
+  // ],
+};
+
+export default function Layout({
+  children,
+}) {
+  return (
+    <>{children}</>
+  );
+}
